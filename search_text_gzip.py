d24c6357e4bfcda4a09f7c35617c17e2ec467737
Piergiorgio Di Pasquale <66187691+joe-pier@users.noreply.github.com>
2023-03-01 11:58:13 +0100

    add support for regex search in gzip files

--- search_text_gzip.py
+++ search_text_gzip.py
@@ -23,7 +23,7 @@ def get_items(txt_file_name):
 
 
 def search_append_items(df_append_items):
-    with ThreadPoolExecutor(max_workers=1000) as executor:
+    with ThreadPoolExecutor(max_workers=64) as executor:
         items = list(executor.map(get_items, list(df_append_items.path)))
     df_append_items["items"] = items
     return df_append_items
@@ -38,7 +38,7 @@ def get_AGM_date(txt_file_name):
 
 
 def search_append_date_AGMs(df_append_date_AGMs):
-    with ThreadPoolExecutor(max_workers=1000) as executor:
+    with ThreadPoolExecutor(max_workers=64) as executor:
         AGM_date = list(executor.map(
             get_AGM_date, list(df_append_date_AGMs.path)))
 
@@ -47,49 +47,46 @@ def search_append_date_AGMs(df_append_date_AGMs):
 
 ######################################################
 
-def create_dataset():
+def create_dataset(year: str, sample_size: str):
     d = {}
-    path = "test GZIP FILES/"
-    dl = os.listdir(path)
+    path = f"GZIP_y{year}_s{sample_size}/"
+    dl = os.listdir(path)[1::] #exclude the compression report
     dl_path = [path+i for i in dl]
     d["path"]= dl_path
 
     df = pd.DataFrame(d)
-    df.to_csv("path_dataframe.csv")
+    df.to_csv(f".gzip_paths/gzip_paths_y{year}_s{sample_size}.csv")
 
 
 ######################################################
 
 
 def main():
-    df = pd.read_csv("path_dataframe.csv")
+    df = pd.read_csv(f".gzip_paths/gzip_paths_y{year}_s{sample_size}.csv")
+    df.drop(list(df.filter(regex='Unnamed')), axis=1, inplace=True)
+
+    # append items
     start_item_time = time.time()
     year_filtered_df_with_items = search_append_items(df).reset_index(drop=True)
     end_item_time = time.time()
     print("time for item extraction operation", end_item_time-start_item_time)
 
-    # append date for AGMs:
+    # append bool AGMs:
     start_AGM_time = time.time()
-    year_filtered_df_with_items_AGM_date = search_append_date_AGMs(
-        year_filtered_df_with_items)
+    year_filtered_df_with_items_AGM_date = search_append_date_AGMs(year_filtered_df_with_items)
     end_AGM_time = time.time()
     print("time for AGM extraction operation", end_AGM_time-start_AGM_time)
-    year_filtered_df_with_items_AGM_date.to_csv(
-        f"OUTPUT/test.csv")
+    year_filtered_df_with_items_AGM_date.to_csv(f".OUTPUT/AGMs_excomp_y{year}_s{sample_size}.csv")
     
-
-def test():
-    test = pd.read_csv("OUTPUT/df_yfilter_sample_100_2016_with_items_AGM_date.csv",
-                       parse_dates=["dates"], dtype={"cik": str})
-    test.drop(list(test.filter(regex='Unnamed')), axis=1, inplace=True)
-
-    test.cik = test.cik.str.zfill(10)
-    test["year"] = pd.DatetimeIndex(test['dates']).year
-
-    print(test)
 
 
 
 if __name__ == "__main__":
-    create_dataset()
-    main()
+    year = 2016
+    sample_size = 1000
+    try:
+        create_dataset(year = year, sample_size=sample_size)
+    except:
+        print(f"no folder named gzip_paths_y{year}_s{sample_size}.csv")
+    finally:
+        main()
