04ed6406054de909ef527ca989e23baeda4227e2
Piergiorgio Di Pasquale <66187691+joe-pier@users.noreply.github.com>
2023-03-16 22:45:13 +0100

    update gitignore

--- get_def_14_a.py
+++ get_def_14_a.py
@@ -6,8 +6,8 @@ import os
 
 
 def get_def14a_year_qrt(year, qrt):
-    df = pd.read_csv(f".masterfiles\master_{year}_{qrt}\master_{year}_{qrt}.csv", header=[
-                     0], delimiter="|", quoting=csv.QUOTE_NONE, encoding='utf-8', index_col=0)
+    df = pd.read_csv(f".masterfiles\master_{year}_{qrt}\master_{year}_{qrt}.csv", header=
+                     [0], delimiter="|", quoting=csv.QUOTE_NONE, encoding='utf-8', index_col=0)
 
     df = df[df['Company Name'].notna()].reset_index(drop=True)
 
@@ -20,12 +20,14 @@ def get_def14a_year_qrt(year, qrt):
 
 
 def retrive_documents(df, year, qrt):
+    
     os.mkdir(f"DEF14A_{year}_{qrt}")
     for n, company in enumerate(df['fname']):
-        print(df['fname'][n])
-        url = "https://www.sec.gov/Archives/"
+        print(n, len(df),df['fname'][n])
+        url = "https://www.sec.gov/Archives/edgar/data/1013706/0001171843-17-002290.txt"
         r = requests.get(url+company, stream=True,
                          headers={'User-agent': 'Mozilla/5.0'})
+        print(r.status_code)
         if r.status_code == 200:
             with open(f"DEF14A_{year}_{qrt}/"+'serial__'+str(n)+"__CIK__"+company[11:18].replace("/", "_")+'__date__'+list(df['dfiled'])[n]+'.txt', 'wb') as f:
                 r.raw.decode_content = True
@@ -33,8 +35,7 @@ def retrive_documents(df, year, qrt):
 
 
 if __name__ == "__main__":
-    year, qrt = [2017, "QTR1"]
+    year, qrt = [2017, "QTR3"]
 
     df = get_def14a_year_qrt(year, qrt)
-
     retrive_documents(df, year, qrt)
